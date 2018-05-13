1d0ce8ca2ac32ae79b0d211a524bb6361d9d5906
Angelo Feraudo <aferaudo34@gmail.com>
2018-05-13 21:36:36 +0200

    Add file heuristics.py

---
+++ src/gameImplementations/heuristics.py
@@ -0,0 +1,7 @@
+"""This file contains the heuristics of the game
+For example:
+Heuristic that looks at the number of pieces and the potential mills
+that could be formed
+
+This function (eval_fun) return a value that can be used by alpha beta algotithm
+"""
